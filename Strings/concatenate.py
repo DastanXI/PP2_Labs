93d1b2bccabde9f019dfc9f4197580a09fbc0d15
DastanXI <dastan2k6@gmail.com>
2025-06-03 16:30:42 +0500

    Добавлены все коды и папки

---
+++ Strings/concatenate.py
@@ -0,0 +1,19 @@
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
---
+++ Strings/slicing.py
@@ -0,0 +1,12 @@
+#b = "Hello, World!"
+#print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5]) #starts at the first
+
+
+b = "Hello, World!"
+print(b[2:]) #till the end of the string
+
+b = "Hello, World!"
+print(b[-5:-2])
